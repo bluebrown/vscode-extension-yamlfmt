24b940b94c97096bdbd134968bb005f7dc5838d1
Kenichi Kamiya <kachick1@gmail.com>
2024-05-03 22:45:30 +0900

    chore(test): avoid project .yamlfmt pollution while running tests

--- test/runTest.js
+++ test/runTest.js
@@ -1,9 +1,19 @@
 const path = require("path");
 const os = require("os");
+const fs = require("fs");
 
 const { runTests } = require("@vscode/test-electron");
 
+const projectConfigPath = ".yamlfmt";
+const backupConfigPath = `${projectConfigPath}.bak`;
+
 async function main() {
+  fs.rename(projectConfigPath, backupConfigPath, function (err) {
+    if (err) {
+      throw err;
+    }
+  });
+
   try {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
@@ -24,6 +34,12 @@ async function main() {
     console.error("Failed to run tests", err);
     process.exit(1);
   }
+
+  fs.rename(backupConfigPath, projectConfigPath, function (err) {
+    if (err) {
+      throw err;
+    };
+  });
 }
 
 main();
